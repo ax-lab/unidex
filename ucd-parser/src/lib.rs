eee8494e5f433609ae5ec6fd1f27ac87596a00dd
ax-lab <98651633+ax-lab@users.noreply.github.com>
2022-04-16 19:37:33 -0300

    doc(parser): add comment describing the crate

--- ucd-parser/src/lib.rs
+++ ucd-parser/src/lib.rs
@@ -1,8 +1,10 @@
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		let result = 2 + 2;
-		assert_eq!(result, 4);
-	}
-}
+//! Provides utilities for loading Unicode Character Database (UCD) files
+//! and parsing the data.
+//!
+//! This is NOT meant to be used at runtime in production systems. Instead,
+//! the purpose of this crate is to be used as part of the build process
+//! when generating Unicode related code.
+//!
+//! The above has consequences particularly for error handling. For simplicity
+//! sake and ease of use, most errors are handled with simple assertions and
+//! panics.
