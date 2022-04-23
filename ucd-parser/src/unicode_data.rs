11ee0099d331bfc09a3899ce9eab52490e36eec8
ax-lab <98651633+ax-lab@users.noreply.github.com>
2022-04-23 12:47:30 -0300

    refactor(UnicodeData): remove get methods
    - change `UnicodeData` fields to have direct access

--- ucd-parser/src/unicode_data.rs
+++ ucd-parser/src/unicode_data.rs
@@ -1,141 +1,57 @@
 use super::data::*;
 
 pub struct UnicodeData<'a> {
-	code: u32,
-	name: &'a str,
-	category: Category,
-	combining_class: u8,
-	bidi: Bidi,
-	decomposition: Option<Decomposition>,
-	decimal_value: DecimalValue,
-	digit_value: DigitValue,
-	numeric_value: NumericValue,
-	mirrored: Mirrored,
-	unicode_old_name: &'a str,
-	iso_10646_comment: &'a str,
-	uppercase_mapping: CaseMapping,
-	lowercase_mapping: CaseMapping,
-	titlecase_mapping: CaseMapping,
-}
-
-impl<'a> UnicodeData<'a> {
-	pub fn new(
-		code: u32,
-		name: &'a str,
-		category: Category,
-		combining_class: u8,
-		bidi: Bidi,
-		decomposition: Option<Decomposition>,
-		decimal_value: DecimalValue,
-		digit_value: DigitValue,
-		numeric_value: NumericValue,
-		mirrored: Mirrored,
-		unicode_old_name: &'a str,
-		iso_10646_comment: &'a str,
-		uppercase_mapping: CaseMapping,
-		lowercase_mapping: CaseMapping,
-		titlecase_mapping: CaseMapping,
-	) -> Self {
-		UnicodeData {
-			code,
-			name,
-			category,
-			combining_class,
-			bidi,
-			decomposition,
-			decimal_value,
-			digit_value,
-			numeric_value,
-			mirrored,
-			unicode_old_name,
-			iso_10646_comment,
-			uppercase_mapping,
-			lowercase_mapping,
-			titlecase_mapping,
-		}
-	}
-
 	/// Codepoint value. Note that for codepoint ranges this can represent the
 	/// start or end of a range of codepoints.
-	pub fn code(&self) -> u32 {
-		self.code
-	}
+	pub code: u32,
 
 	/// Character name.
-	pub fn name(&self) -> &'a str {
-		self.name
-	}
+	pub name: &'a str,
 
 	/// General category for the character.
-	pub fn category(&self) -> Category {
-		self.category
-	}
+	pub category: Category,
 
 	/// The classes used for the Canonical Ordering Algorithm in the Unicode
 	/// standard.
-	pub fn combining_class(&self) -> u8 {
-		self.combining_class
-	}
+	pub combining_class: u8,
 
 	/// Bidirectional category for this character.
-	pub fn bidi(&self) -> Bidi {
-		self.bidi
-	}
+	pub bidi: Bidi,
 
 	/// Decomposition mapping for the character.
-	pub fn decomposition(&self) -> &Option<Decomposition> {
-		&self.decomposition
-	}
+	pub decomposition: Option<Decomposition>,
 
 	/// Decimal digit value, if the character has the decimal digit property.
-	pub fn decimal_value(&self) -> DecimalValue {
-		self.decimal_value
-	}
+	pub decimal_value: DecimalValue,
 
 	/// Digit value, if the character represents a digit, not necessarily a
 	/// decimal digit.
-	pub fn digit_value(&self) -> DigitValue {
-		self.digit_value
-	}
+	pub digit_value: DigitValue,
 
 	/// Numeric value, if the character has the numeric property.
-	pub fn numeric_value(&self) -> NumericValue {
-		self.numeric_value
-	}
+	pub numeric_value: NumericValue,
 
 	/// If the character has been identified as a "mirrored" character in
 	/// bidirectional text this is [`Mirrored::Yes`].
-	pub fn mirrored(&self) -> Mirrored {
-		self.mirrored
-	}
+	pub mirrored: Mirrored,
 
 	/// Old name as published in Unicode 1.0. This name is only provided
 	/// when it is significantly different from the Unicode 3.0 name for
 	/// the character.
-	pub fn unicode_old_name(&self) -> &'a str {
-		self.unicode_old_name
-	}
+	pub unicode_old_name: &'a str,
 
 	/// This is the ISO 10646 comment field. It is in parantheses in the 10646
 	/// names list.
-	pub fn iso_10646_comment(&self) -> &'a str {
-		self.iso_10646_comment
-	}
+	pub iso_10646_comment: &'a str,
 
 	/// Uppercase mapping for this character.
-	pub fn uppercase_mapping(&self) -> CaseMapping {
-		self.uppercase_mapping
-	}
+	pub uppercase_mapping: CaseMapping,
 
 	/// Lowercase mapping for this character.
-	pub fn lowercase_mapping(&self) -> CaseMapping {
-		self.lowercase_mapping
-	}
+	pub lowercase_mapping: CaseMapping,
 
 	/// Titlecase mapping for this character.
-	pub fn titlecase_mapping(&self) -> CaseMapping {
-		self.titlecase_mapping
-	}
+	pub titlecase_mapping: CaseMapping,
 }
 
 impl<'a> std::fmt::Display for UnicodeData<'a> {
@@ -243,84 +159,84 @@ mod tests {
 			tag: Some(DecompositionTag::Font),
 			codes: vec![10, 20, 30],
 		};
-		let entry = UnicodeData::new(
-			0x12AB,
-			"char name",
-			Category::LetterLowercase,
-			230,
-			Bidi::L,
-			Some(decomposition.clone()),
-			DecimalValue::Some(1),
-			DigitValue::Some(1),
-			NumericValue::Rational(1, 5),
-			Mirrored::Yes,
-			"unicode 1.0 name",
-			"ISO 10646 comment",
-			CaseMapping::Some(1),
-			CaseMapping::Some(2),
-			CaseMapping::None,
-		);
-		assert_eq!(entry.code(), 0x12AB);
-		assert_eq!(entry.name(), "char name");
-		assert_eq!(entry.category(), Category::LetterLowercase);
-		assert_eq!(entry.combining_class(), 230);
-		assert_eq!(entry.bidi(), Bidi::L);
-		assert_eq!(entry.decomposition(), &Some(decomposition));
-		assert_eq!(entry.decimal_value(), DecimalValue::Some(1));
-		assert_eq!(entry.digit_value(), DigitValue::Some(1));
-		assert_eq!(entry.numeric_value(), NumericValue::Rational(1, 5));
-		assert_eq!(entry.mirrored(), Mirrored::Yes);
-		assert_eq!(entry.unicode_old_name(), "unicode 1.0 name");
-		assert_eq!(entry.iso_10646_comment(), "ISO 10646 comment");
-		assert_eq!(entry.uppercase_mapping(), CaseMapping::Some(1));
-		assert_eq!(entry.lowercase_mapping(), CaseMapping::Some(2));
-		assert_eq!(entry.titlecase_mapping(), CaseMapping::None);
+		let entry = UnicodeData {
+			code: 0x12AB,
+			name: "char name",
+			category: Category::LetterLowercase,
+			combining_class: 230,
+			bidi: Bidi::L,
+			decomposition: Some(decomposition.clone()),
+			decimal_value: DecimalValue::Some(1),
+			digit_value: DigitValue::Some(1),
+			numeric_value: NumericValue::Rational(1, 5),
+			mirrored: Mirrored::Yes,
+			unicode_old_name: "unicode 1.0 name",
+			iso_10646_comment: "ISO 10646 comment",
+			uppercase_mapping: CaseMapping::Some(1),
+			lowercase_mapping: CaseMapping::Some(2),
+			titlecase_mapping: CaseMapping::None,
+		};
+		assert_eq!(entry.code, 0x12AB);
+		assert_eq!(entry.name, "char name");
+		assert_eq!(entry.category, Category::LetterLowercase);
+		assert_eq!(entry.combining_class, 230);
+		assert_eq!(entry.bidi, Bidi::L);
+		assert_eq!(entry.decomposition, Some(decomposition));
+		assert_eq!(entry.decimal_value, DecimalValue::Some(1));
+		assert_eq!(entry.digit_value, DigitValue::Some(1));
+		assert_eq!(entry.numeric_value, NumericValue::Rational(1, 5));
+		assert_eq!(entry.mirrored, Mirrored::Yes);
+		assert_eq!(entry.unicode_old_name, "unicode 1.0 name");
+		assert_eq!(entry.iso_10646_comment, "ISO 10646 comment");
+		assert_eq!(entry.uppercase_mapping, CaseMapping::Some(1));
+		assert_eq!(entry.lowercase_mapping, CaseMapping::Some(2));
+		assert_eq!(entry.titlecase_mapping, CaseMapping::None);
 	}
 
 	#[test]
 	fn supports_to_string() {
-		let entry = UnicodeData::new(
-			0x12AB,
-			"some name",
-			Category::LetterUppercase,
-			230,
-			Bidi::L,
-			Some(Decomposition {
+		let entry = UnicodeData {
+			code: 0x12AB,
+			name: "some name",
+			category: Category::LetterUppercase,
+			combining_class: 230,
+			bidi: Bidi::L,
+			decomposition: Some(Decomposition {
 				tag: Some(DecompositionTag::Font),
 				codes: vec![0x10, 0x20, 0x30],
 			}),
-			DecimalValue::Some(1),
-			DigitValue::Some(2),
-			NumericValue::Rational(1, 5),
-			Mirrored::Yes,
-			"old name",
-			"iso name",
-			CaseMapping::Some(0xA1),
-			CaseMapping::Some(0xB2),
-			CaseMapping::Some(0xC3),
-		);
+			decimal_value: DecimalValue::Some(1),
+			digit_value: DigitValue::Some(2),
+			numeric_value: NumericValue::Rational(1, 5),
+			mirrored: Mirrored::Yes,
+			unicode_old_name: "old name",
+			iso_10646_comment: "iso name",
+			uppercase_mapping: CaseMapping::Some(0xA1),
+			lowercase_mapping: CaseMapping::Some(0xB2),
+			titlecase_mapping: CaseMapping::Some(0xC3),
+		};
 		assert_eq!(
 			entry.to_string(),
 			"12AB;some name;Lu;230;L;<font> 0010 0020 0030;1;2;1/5;Y;old name;iso name;00A1;00B2;00C3"
 		);
 
-		let entry = UnicodeData::new(
-			0xFF,
-			"other name",
-			Category::MarkEnclosing,
-			0,
-			Bidi::LRE,
-			None,
-			DecimalValue::None,
-			DigitValue::None,
-			NumericValue::None,
-			Mirrored::No,
-			"old",
-			"",
-			CaseMapping::None,
-			CaseMapping::None,
-			CaseMapping::None,
-		);
+		let entry = UnicodeData {
+			code: 0xFF,
+			name: "other name",
+			category: Category::MarkEnclosing,
+			combining_class: 0,
+			bidi: Bidi::LRE,
+			decomposition: None,
+			decimal_value: DecimalValue::None,
+			digit_value: DigitValue::None,
+			numeric_value: NumericValue::None,
+			mirrored: Mirrored::No,
+			unicode_old_name: "old",
+			iso_10646_comment: "",
+			uppercase_mapping: CaseMapping::None,
+			lowercase_mapping: CaseMapping::None,
+			titlecase_mapping: CaseMapping::None,
+		};
 		assert_eq!(entry.to_string(), "00FF;other name;Me;0;LRE;;;;;N;old;;;;");
 	}
 }
