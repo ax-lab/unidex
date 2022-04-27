238c98cf6f95600b58afc680256126b7292c543d
ax-lab <98651633+ax-lab@users.noreply.github.com>
2022-04-27 14:48:35 -0300

    wip(properties): add key/value storage in range
    - allow storing key/values for multiple properties in range row
    - basic logic for matching the same range

--- property-ranges/src/table.rs
+++ property-ranges/src/table.rs
@@ -1,16 +1,22 @@
-use std::ops::{Range, RangeInclusive};
+use std::{
+	any::Any,
+	ops::{Range, RangeInclusive},
+};
 
 pub struct PropertyTable {
-	range: Option<RangeInclusive<u32>>,
+	ranges: Vec<PropertyRange>,
 }
 
 impl PropertyTable {
 	pub fn new() -> Self {
-		PropertyTable { range: None }
+		PropertyTable {
+			ranges: Default::default(),
+		}
 	}
 
 	pub fn count(&self) -> usize {
-		if let Some(range) = &self.range {
+		if let Some(row) = self.ranges.iter().next() {
+			let range = &row.range;
 			(range.end() - range.start()) as usize + 1
 		} else {
 			0
@@ -18,20 +24,29 @@ impl PropertyTable {
 	}
 
 	pub fn count_ranges(&self) -> usize {
-		1
+		self.ranges.len()
 	}
 
-	pub fn get_range<T: PropertyKey>(&self, _index: usize, _key: T) -> PropertyValue<T::Value> {
-		PropertyValue(self.range.as_ref().unwrap().clone(), T::Value::get_some())
+	pub fn get_range(&self, index: usize) -> &PropertyRange {
+		&self.ranges[index]
 	}
 
 	pub fn set_range<R: CodeRange, T: PropertyKey>(
 		&mut self,
 		range: R,
-		_key: T,
-		_property: T::Value,
+		key: T,
+		property: T::Value,
 	) {
-		self.range = Some(RangeInclusive::new(range.start(), range.end_inclusive()));
+		let range = RangeInclusive::new(range.start(), range.end_inclusive());
+		for it in self.ranges.iter_mut() {
+			if it.range == range {
+				it.values.push((key.as_any(), T::value_to_any(property)));
+				return;
+			}
+		}
+		let mut range = PropertyRange::new(range);
+		range.values.push((key.as_any(), T::value_to_any(property)));
+		self.ranges.push(range);
 	}
 }
 
@@ -41,26 +56,44 @@ impl Default for PropertyTable {
 	}
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct PropertyValue<T>(pub RangeInclusive<u32>, pub T);
-
-pub trait PropertyKey {
-	type Value: SomeValue;
+pub struct PropertyRange {
+	pub range: RangeInclusive<u32>,
+	values: Vec<(Box<dyn Any>, Box<dyn Any>)>,
 }
 
-pub trait SomeValue {
-	fn get_some() -> Self;
-}
+impl PropertyRange {
+	pub(crate) fn new(range: RangeInclusive<u32>) -> Self {
+		PropertyRange {
+			range,
+			values: Default::default(),
+		}
+	}
 
-impl SomeValue for u32 {
-	fn get_some() -> Self {
-		42
+	pub fn get<T: PropertyKey + 'static>(&self, key: T) -> T::Value {
+		for (my_key, val) in self.values.iter() {
+			if let Some(my_key) = my_key.downcast_ref::<T>() {
+				if &key == my_key {
+					let val = val.downcast_ref::<T::Value>();
+					return val.unwrap().clone();
+				}
+			}
+		}
+		panic!()
 	}
 }
 
-impl SomeValue for &'static str {
-	fn get_some() -> Self {
-		"some property"
+#[derive(Debug, PartialEq, Eq)]
+pub struct PropertyValue<T>(pub RangeInclusive<u32>, pub T);
+
+pub trait PropertyKey: PartialEq + Clone + 'static {
+	type Value: Clone;
+
+	fn as_any(&self) -> Box<dyn Any> {
+		Box::new(self.clone())
+	}
+
+	fn value_to_any(value: Self::Value) -> Box<dyn Any> {
+		Box::new(value)
 	}
 }
 
@@ -106,13 +139,15 @@ mod tests {
 	}
 
 	#[test]
-	fn can_store_single_range_with_single_property() {
+	fn stores_single_range_with_single_property() {
+		#[derive(Clone, PartialEq)]
 		struct SomeKeyA;
 
 		impl PropertyKey for SomeKeyA {
 			type Value = &'static str;
 		}
 
+		#[derive(Clone, PartialEq)]
 		struct SomeKeyB;
 
 		impl PropertyKey for SomeKeyB {
@@ -123,20 +158,64 @@ mod tests {
 		table_a.set_range(1..=255, SomeKeyA, "some property");
 		assert_eq!(table_a.count(), 255);
 		assert_eq!(table_a.count_ranges(), 1);
-		assert_eq!(
-			table_a.get_range(0, SomeKeyA),
-			PropertyValue(1..=255, "some property")
-		);
+
+		let range_a = table_a.get_range(0);
+		assert_eq!(range_a.range, 1..=255);
+		assert_eq!(range_a.get(SomeKeyA), "some property");
 
 		let mut table_b = PropertyTable::new();
 		table_b.set_range(0..=9, SomeKeyB, 42);
 		assert_eq!(table_b.count(), 10);
 		assert_eq!(table_b.count_ranges(), 1);
-		assert_eq!(table_b.get_range(0, SomeKeyB), PropertyValue(0..=9, 42u32));
+
+		let range_b = table_b.get_range(0);
+		assert_eq!(range_b.range, 0..=9);
+		assert_eq!(range_b.get(SomeKeyB), 42u32);
+	}
+
+	#[test]
+	fn stores_multiple_properties() {
+		#[derive(Clone, PartialEq)]
+		struct Key(&'static str);
+
+		impl PropertyKey for Key {
+			type Value = &'static str;
+		}
+
+		let mut table = PropertyTable::new();
+		table.set_range(0..=9, Key("a"), "value a");
+		table.set_range(0..=9, Key("b"), "value b");
+
+		let range = table.get_range(0);
+		assert_eq!(range.get(Key("a")), "value a");
+		assert_eq!(range.get(Key("b")), "value b");
+	}
+
+	#[test]
+	fn stores_multiple_ranges() {
+		#[derive(Clone, PartialEq)]
+		struct Key;
+
+		impl PropertyKey for Key {
+			type Value = u32;
+		}
+
+		let mut table = PropertyTable::new();
+		table.set_range(10..=19, Key, 1);
+		table.set_range(20..=29, Key, 2);
+		assert_eq!(table.count_ranges(), 2);
+
+		let a = table.get_range(0);
+		let b = table.get_range(1);
+		assert_eq!(a.range, 10..=19);
+		assert_eq!(b.range, 20..=29);
+		assert_eq!(a.get(Key), 1);
+		assert_eq!(b.get(Key), 2);
 	}
 
 	#[test]
 	fn supports_non_inclusive_range() {
+		#[derive(Clone, PartialEq)]
 		struct Key;
 
 		impl PropertyKey for Key {
@@ -145,6 +224,6 @@ mod tests {
 
 		let mut table = PropertyTable::new();
 		table.set_range(0..10, Key, 42);
-		assert_eq!(table.get_range(0, Key), PropertyValue(0..=9, 42));
+		assert_eq!(table.get_range(0).range, 0..=9);
 	}
 }
